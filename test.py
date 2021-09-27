d320500b75b82b611aad4c9f399350d618d20ee9
Srilalitha Vishnubhotla <lolboch@gmail.com>
2021-09-27 09:01:37 +0530

    added gridsearchcv

---
+++ test.py
@@ -0,0 +1,25 @@
+from numpy import arange
+from pandas import read_csv
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RepeatedKFold
+from sklearn.linear_model import ElasticNet
+# load the dataset
+url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/housing.csv'
+dataframe = read_csv(url, header=None)
+data = dataframe.values
+X, y = data[:, :-1], data[:, -1]
+# define model
+model = ElasticNet()
+# define model evaluation method
+cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+# define grid
+grid = dict()
+grid['alpha'] = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.0, 1.0, 10.0, 100.0]
+grid['l1_ratio'] = arange(0, 1, 0.01)
+# define search
+search = GridSearchCV(model, grid, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
+# perform the search
+results = search.fit(X, y)
+# summarize
+print('MAE: %.3f' % results.best_score_)
+print('Config: %s' % results.best_params_)
